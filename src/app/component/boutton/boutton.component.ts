d2c15137c1e299b3de08e13c4b7a65cff8360a9a
LouisLamoureux1 <louis.lamoureux@laposte.net>
2025-03-18 12:18:35 +0100

    component button

---
+++ src/app/component/boutton/boutton.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boutton',
+  standalone: false,
+  templateUrl: './boutton.component.html',
+  styleUrl: './boutton.component.css'
+})
+export class BouttonComponent {
+
+}
